efaeee55cc1f4d5c5770e0e886458fbe27dcf342
Yashodhan <yashodhanghadge@gmail.com>
2018-06-01 09:31:11 +0530

    Changed the vgg network to add hypercolumn descriptors

--- models/VGG.py
+++ models/VGG.py
@@ -24,7 +24,53 @@ class Vgg16(torch.nn.Module):
             for param in self.parameters():
                 param.requires_grad = False
 
-    def forward(self, X):
+        # these layers are used as replacement for the last 3 FC layers of the VGG 16 net.
+
+        self.slice5 = torch.nn.Sequential(
+            torch.nn.Conv2d(512, 4096, kernel_size=7),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.5)
+        )
+        self.slice6 = torch.nn.Sequential(
+            torch.nn.Conv2d(4096, 4096, kernel_size=1),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.5)
+        )
+        self.upsample = torch.nn.Upsample(self.input_dims, mode='bilinear')
+
+        self.MLP_Layer = torch.nn.Sequential(
+            torch.nn.Linear(self.sample_size * 960, 4096),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(4096, self.sample_size),
+            torch.nn.Dropout(p=0.5)
+        )
+
+    def extract_hypercoloumn(self, network_output_layers, layer_indices=None, input_image=None, pixel_set=None):
+        hypercolumns = torch.Tensor()
+        layers = []
+        if layer_indices is not None:
+            for index in layer_indices:
+                layers.append(network_output_layers[index])
+        else:
+            layers = network_output_layers
+
+        for layer in layers:
+            # layer = layer.to('cpu')
+            for _filter in layer[0][:]:
+                hypercolumns.append(_filter.detach().numpy())
+
+        hypercolumns = np.array(hypercolumns)
+        print('hypercolumns shape : {}'.format(hypercolumns.shape))
+        k = []
+        if pixel_set is not None:
+            for i, j in pixel_set[:, 1:]:
+                k.append(hypercolumns[:, i, j])
+            return torch.from_numpy(np.array(k)).to('cuda:0')
+        return hypercolumns
+
+    def forward(self, X, pixel_samples=None):
+        self.outputs = []
         h = self.slice1(X)
         h_relu1_2 = h
         h = self.slice2(h)

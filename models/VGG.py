60aced2a4992a7abc1100e8108d5f2a649f664e7
Yashodhan <yashodhanghadge@gmail.com>
2018-06-04 12:43:10 +0530

    implemented full working code for training pixelnet

--- models/VGG.py
+++ models/VGG.py
@@ -1,12 +1,16 @@
+import numpy as np
 import torch
 from torchvision import models
-from collections import namedtuple
 
 
 class Vgg16(torch.nn.Module):
 
-    def __init__(self, requires_grad=False):
+    def __init__(self, input_dims, sample_size=None, requires_grad=False):
         super(Vgg16, self).__init__()
+        self.input_dims = input_dims
+        if sample_size is not None:
+            self.sample_size = sample_size
+
         vgg_pretrained_features = models.vgg16(pretrained=True).features
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
@@ -21,7 +25,7 @@ class Vgg16(torch.nn.Module):
         for x in range(16, 23):
             self.slice4.add_module(str(x), vgg_pretrained_features[x])
         if not requires_grad:
-            for param in self.parameters():
+            for name, param in self.named_children():
                 param.requires_grad = False
 
         # these layers are used as replacement for the last 3 FC layers of the VGG 16 net.
@@ -46,40 +50,100 @@ class Vgg16(torch.nn.Module):
             torch.nn.Dropout(p=0.5)
         )
 
-    def extract_hypercoloumn(self, network_output_layers, layer_indices=None, input_image=None, pixel_set=None):
-        hypercolumns = torch.Tensor()
-        layers = []
-        if layer_indices is not None:
-            for index in layer_indices:
-                layers.append(network_output_layers[index])
-        else:
-            layers = network_output_layers
+        self.comp_rate = [1, 2, 4, 8, 16, 224]
+        self.padding_rate = []
+        for comp in self.comp_rate:
+            self.padding_rate.append(comp-1.0/2)
 
-        for layer in layers:
-            # layer = layer.to('cpu')
-            for _filter in layer[0][:]:
-                hypercolumns.append(_filter.detach().numpy())
+        self.dtype = torch.cuda.FloatTensor
+    def c_i(self, pixels_set, layers):
+        hypercols_for_batch = torch.cuda.FloatTensor()
+        for j,batch_item in enumerate(pixels_set):
+            x = batch_item[:, 1]  # gets all the x_coords
+            y = batch_item[:, 2]  # gets all the y_coords
+            hypercols = torch.cuda.FloatTensor()
+            for i, layer in enumerate(layers):
+                layer_height = layer[j].size(1)
+                layer_width = layer[j].size(2)
+                tx = (x.type(self.dtype) - self.padding_rate[i]) / self.comp_rate[i]
+                ty = (y.type(self.dtype) - self.padding_rate[i]) / self.comp_rate[i]
+                tx1 = torch.floor(tx).type(torch.cuda.LongTensor)
+                ty1 = torch.floor(ty).type(torch.cuda.LongTensor)
+                # tx1 = tx - 1
+                # ty1 = ty - 1
 
-        hypercolumns = np.array(hypercolumns)
-        print('hypercolumns shape : {}'.format(hypercolumns.shape))
-        k = []
-        if pixel_set is not None:
-            for i, j in pixel_set[:, 1:]:
-                k.append(hypercolumns[:, i, j])
-            return torch.from_numpy(np.array(k)).to('cuda:0')
-        return hypercolumns
+                tx2 = tx1 + 1
+                ty2 = ty1 + 1
+
+                tx1 = torch.clamp(tx1, 0, layer_width - 1)
+                tx2 = torch.clamp(tx2, 0, layer_width - 1)
+                ty1 = torch.clamp(ty1, 0, layer_height - 1)
+                ty2 = torch.clamp(ty2, 0, layer_height - 1)
+
+                Ia = layer[j][:, ty1, tx1]
+                Ib = layer[j][:, ty2, tx1]
+                Ic = layer[j][:, ty1, tx2]
+                Id = layer[j][:, ty2, tx2]
+
+                wa = (tx2 - x) * (ty2 - y)
+                wb = (tx2 - x) * (y - ty1)
+                wc = (x - tx1) * (ty2 - y)
+                wd = (x - tx1) * (y - ty1)
+
+                upsampled_layer = wa.type(self.dtype)*Ia + wb.type(self.dtype)*Ib + wc.type(self.dtype)*Ic + wd.type(self.dtype)*Id
+                hypercols = torch.cat((hypercols,upsampled_layer.unsqueeze(0)),1)
+            hypercols_for_batch = torch.cat((hypercols,hypercols_for_batch))
+        return hypercols_for_batch
+
+        pass
+
+    def extract_hypercoloumn(self, network_output_filters, layer_indices=None, input_image=None, pixel_set=None):
+
+        # for layer in layers:
+        #     # layer = layer.to('cpu')
+        #     hypercolumns.append(layer[0])
+        #     # for _filter in layer[0][:]:
+        #     #     print (_filter.shape)
+        #     #     hypercolumns = torch.stack((hypercolumns,_filter),0)
+        """
+            pixel_set = (N,size,coords)
+            coords = (z,x,y)
+
+        """
+        hypercols = torch.cuda.FloatTensor()
+        for i, batch_item in enumerate(network_output_filters):
+            if pixel_set is not None:
+                pts = pixel_set[0, :, 1:]
+                k = batch_item[:, pts[:, 0], pts[:, 1]]
+                hypercols = torch.cat((hypercols, k.unsqueeze(0)))
+        return hypercols
 
     def forward(self, X, pixel_samples=None):
-        self.outputs = []
+        outputs = []
         h = self.slice1(X)
-        h_relu1_2 = h
+        # h1 = self.upsample(h)
+        outputs.append(h)
         h = self.slice2(h)
-        h_relu2_2 = h
+        # h2 = self.upsample(h)
+        outputs.append(h)
+
         h = self.slice3(h)
-        h_relu3_3 = h
+        # h3 = self.upsample(h)
+        outputs.append(h)
         h = self.slice4(h)
-        h_relu4_3 = h
-        # vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
-        # out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
-        out = h_relu4_3
-        return out
+        # h4 = self.upsample(h)
+        outputs.append(h)
+        # outputs = torch.cat((h1, h2,h3,h4), 1)
+
+        h = self.slice5(h)
+
+        h = self.slice6(h)
+
+        hypercols = self.c_i(pixel_samples, outputs)
+        # print (hypercols.shape)
+        # hypercols = self.extract_hypercoloumn(outputs, pixel_set=pixel_samples)
+        hypercols = hypercols.view(hypercols.size(0), -1)
+
+        h = self.MLP_Layer(hypercols)
+
+        return h
--- train_vgg.py
+++ train_vgg.py
@@ -1,9 +1,13 @@
 import os
 
-import numpy
 import torch
 import torch.nn as nn
 import torchvision
+
+import datetime
+
+import numpy as np
+
 from PIL import Image
 from torchvision import transforms
 
@@ -13,13 +17,15 @@ import torch.utils.data as datautil
 
 
 def main():
-    net = Vgg16().cuda()
+    input_dims = (128, 128)
+    size = 25
+    net = Vgg16(input_dims, sample_size=size).cuda()
 
     transformations = transforms.Compose(
         [
             #  transforms.RandomRotation(degrees=5),
             #  transforms.RandomHorizontalFlip(p=0.2),
-            transforms.Resize((100, 100)),
+            transforms.Resize(input_dims),
             transforms.ToTensor()
         ]
     )
@@ -27,81 +33,148 @@ def main():
     customDataset = CustomDataset("train", r"C:\Users\Admin\AppData\Local\lxss\home\codexetreme\Lung_CXR\train",
                                   transformations)
     train_loader = datautil.DataLoader(dataset=customDataset,
-                                       batch_size=1,
+                                       batch_size=2,
                                        shuffle=True, num_workers=2
                                        )
 
-    customDatasetTest = CustomDataset("test", r"C:\Users\Admin\AppData\Local\lxss\home\codexetreme\Lung_CXR\test",transformations)
+    Train(train_loader, net,size)
 
-    test_loader = datautil.DataLoader(dataset=customDatasetTest,batch_size=2,shuffle=True)
+    # customDatasetTest = CustomDataset("test", r"C:\Users\Admin\AppData\Local\lxss\home\codexetreme\Lung_CXR\test",
+    #                                   transformations)
+    #
+    # test_loader = datautil.DataLoader(dataset=customDatasetTest, batch_size=2, shuffle=True)
+    # net.eval()
 
-    # Train(train_loader, net)
+    # with torch.no_grad():
+    #     correct = 0
+    #     total = 0
+    #     i = 1
+    #     for images, labels in test_loader:
+    #         if i > 0:
+    #             images = images.to(device)
+    #             labels = labels.to(device)
+    #
+    #             out = net(images)
+    #             print(out[0].dtype)
+    #             total += labels.size(0)
+    #             # image = transforms.ToPILImage().__call__(out)
+    #             # image.save('test.')
+    #             # _, predicted = torch.min(out.data, 1)
+    #             # print(predicted)
+    #             # img_tensor = out[0]
+    #             out = out.to('cpu')
+    #             out = torch.Tensor(out).numpy()
+    #             print(type(out))
+    #             out = numpy.reshape(out[0], (512, 48, 3))
+    #             print(out.shape)
+    #             im = Image.fromarray(numpy.uint8(out * 255))
+    #
+    #             # img = Image.fromarray(out[0]).astype(numpy.uint8)
+    #             # print (img)
+    #             # print (os.listdir('.'))
+    #             im.save(r'.\images\test.png')
+    #             # torchvision.utils.save_image(,'test.png')
+    #             i -= 1
+    #         else:
+    #             break
+    #
+    #         # correct += (predicted == labels).sum().item()  # to get the num of correct predictions
 
-    net.eval()
+    # pass
 
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        i = 1
-        for images, labels in test_loader:
-            if i > 0:
-                images = images.to(device)
-                labels = labels.to(device)
+    # print("Total : {}".format(total))
+    # print("Correct : {}".format(correct))
+    # print("Accuracy = {:.4f}%".format(100 * correct / total))
 
-                out = net(images)
-                print(out[0].dtype)
-                total += labels.size(0)
-                # image = transforms.ToPILImage().__call__(out)
-                # image.save('test.')
-                # _, predicted = torch.min(out.data, 1)
-                # print(predicted)
-                # img_tensor = out[0]
-                out = out.to('cpu')
-                out = torch.Tensor(out).numpy()
-                print (type(out))
-                out = numpy.reshape(out[0],(512,48,3))
-                print (out.shape)
-                im = Image.fromarray(numpy.uint8(out*255))
 
-                # img = Image.fromarray(out[0]).astype(numpy.uint8)
-                # print (img)
-                # print (os.listdir('.'))
-                im.save(r'.\images\test.png')
-                # torchvision.utils.save_image(,'test.png')
-                i -= 1
-            else:
-                break
+def deprocess_image(x, selected_filter):
+    # normalize tensor: center on 0., ensure std is 0.1
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
 
-            # correct += (predicted == labels).sum().item()  # to get the num of correct predictions
+    # clip to [0, 1]
+    x += 0.5
+    x = np.clip(x, 0, 1)
 
-            pass
+    # convert to RGB array
+    x *= 255
+    x = x.transpose((1, 2, 0))
+    x = np.clip(x, 0, 255).astype(np.uint8)
+    im = Image.fromarray(x[:, :, selected_filter])
+    ts = datetime.datetime.now().timestamp()
+    im.save(r'.\images\test_%s.png' % ts)
 
-        print("Total : {}".format(total))
-        print("Correct : {}".format(correct))
-        print("Accuracy = {:.4f}%".format(100 * correct / total))
+
+def make_label_image(labels, index):
+    out = labels.to('cpu')
+    # out = out.float()
+    out = torch.Tensor(out[index].float()).np()
+    # print(out.shape)
+    deprocess_image(out, 0)
+
+
+def sample_pixels(gt_batch, size):
+    selected_pixel_values = torch.cuda.LongTensor()
+    locations = torch.cuda.LongTensor()
+
+    for gt in gt_batch:
+        row_col = (gt < 255).nonzero()
+        pixels = gt[row_col[:, 0], row_col[:, 1]]
+        lp = len(pixels)
+        inds = torch.zeros(size).uniform_(0, lp - 1).long()  # this step still runs on CPU try to make it work on GPU
+
+        row_col = row_col[inds]
+        sel_pixels = pixels[row_col[:, 0], row_col[:, 1]]
+
+        selected_pixel_values = torch.cat((selected_pixel_values, sel_pixels.unsqueeze(0)))
+        locations = torch.cat((row_col.unsqueeze(0), locations))
+
+    return selected_pixel_values, locations
 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 num_epochs = 5
 
 
-def Train(train_loader, net, learning_rate=0.001):
+def Train(train_loader, net,size, learning_rate=0.001):
     net.train()
 
     criterion = nn.CrossEntropyLoss()
 
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-    total_step = 5
+    total_step = len(train_loader)
+
     for epoch in range(num_epochs):
         # gives a batch of 64 each iteration
         for i, (images, labels) in enumerate(train_loader):
+
             images = images.to(device)
             labels = labels.to(device)
 
-            out = net(images)
-            print(out.shape)
-            loss = criterion(out, labels)
+            # pixel_locations = np.zeros((0, 3), dtype=np.float32)
+            # modified_labels = np.zeros(0, dtype=np.float32)
+
+            selected_pixel_values, locations = sample_pixels(labels, size)
+
+            # im_ind = i * torch.ones((size, 1))
+            # pix = np.hstack((im_ind, locs))
+            # pixel_locations = np.vstack((pixel_locations, pix))
+            # modified_labels = np.hstack((modified_labels, labs))
+
+            # pixel_locations = torch.from_numpy(pixel_locations)
+
+            # pixel_locations = pixel_locations.to(device)
+            out = net(images, locations)
+            # labels.unsqueeze_(0)
+            out.unsqueeze_(0)
+            out = torch.t(out)
+
+            # print(out)
+            # print(selected_pixel_values)
+
+            loss = criterion(out, selected_pixel_values)
 
             optimizer.zero_grad()
             loss.backward()
@@ -109,9 +182,16 @@ def Train(train_loader, net, learning_rate=0.001):
 
             # output data to terminal
 
-            if (i + 1) % 400 == 0:
+            if (i + 1) % 10 == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+                torch.save({
+                    'epoch': epoch + 1,
+                    'arch': 'VGG-16',
+                    'state_dict': net.state_dict(),
+                    # 'best_prec1': best_prec1,
+                }, 'checkpoint.tar')
 
             pass
 
